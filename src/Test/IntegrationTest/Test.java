7a6d0741feea77f1ede08cf0e79a073567691ba1
Naama Baruch <t-nbaruch@microsoft.com>
2022-05-11 20:15:26 +0300

    add TestLoginAndAddGame

--- src/Test/IntegrationTest/Test.java
+++ src/Test/IntegrationTest/Test.java
@@ -1,4 +1,28 @@
 package Test.IntegrationTest;
 
+import DomainLayer.Enums;
+import ServiceLayer.SystemController;
+import org.junit.Assert;
+import org.junit.Before;
+
+import static org.junit.Assert.assertEquals;
+
 public class Test {
+    private SystemController controller;
+
+    @Before
+    public void initialize(){
+        controller = SystemController.getInstance();
+    }
+
+    @org.junit.Test
+    public void TestLoginAndAddGame() {
+        // Login with existing userName and password - successful login
+
+        Enums.ActionStatus status = controller.LogInUser("Ido","id123");
+        assertEquals("failed - this test should have been success", status, Enums.ActionStatus.SUCCESS);
+
+        status = controller.SignNewReferee("NammaBaruch","naamaBru123@post.ac.il", Enums.RefereeLevel.Secondary);
+        assertEquals("Registration failed - should be a successful registration.", status, Enums.ActionStatus.SUCCESS);
+    }
 }
