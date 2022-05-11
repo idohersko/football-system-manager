74b9543e17ce9d74562a2ce8ccaf7e14740ee55b
Naama Baruch <t-nbaruch@microsoft.com>
2022-05-11 18:12:59 +0300

    update add game UC

--- src/DomainLayer/Exceptions/UserNameDoesNotExist.java
+++
@@ -1,7 +0,0 @@
-package DomainLayer.Exceptions;
-
-// todo - delete if not in use
-
-
-public class UserNameDoesNotExist extends Exception {
-}
--- src/DomainLayer/Exceptions/WrongPassword.java
+++
@@ -1,8 +0,0 @@
-package DomainLayer.Exceptions;
-
-public class WrongPassword extends Exception{
-    // todo - delete if not in use
-//    public WrongPassword(String errorMessage) {
-//        super(errorMessage);
-//    }
-}
