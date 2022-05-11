557b74f4993a837138217a1b7748fdb188d7a303
Darabu273 <dAr8194765>
2022-05-11 15:51:46 +0300

    Referee

--- src/DomainLayer/Users/Referee.java
+++ src/DomainLayer/Users/Referee.java
@@ -4,10 +4,9 @@ import DomainLayer.Enums;
 import java.util.ArrayList;
 
 public class Referee extends AUser {
-
     public Enums.RefereeLevel refereeLevel;
 
-    public ArrayList<Season> seasons = new ArrayList<>();
+    public ArrayList<String> seasons = new ArrayList<>();
     public ArrayList<Enums.EventType> events = new ArrayList<>();
 
     public Referee(String name, String password, Enums.RefereeLevel refereeLevel) {
