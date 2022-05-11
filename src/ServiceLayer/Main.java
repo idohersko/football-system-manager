d527e03d2d07222ace9652d7b631bf983b9b483e
idohersko <idohersko@gmail.com>
2022-05-11 18:11:00 +0300

    database done

--- src/ServiceLayer/Main.java
+++ src/ServiceLayer/Main.java
@@ -22,11 +22,12 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws SQLException {
-        Referee ref = new Referee("naama","naama123", Enums.ActivationStatus.ACTIVE,Enums.UserType.Referee,Enums.RefereeLevel.Primary);
         UsersDB instance = UsersDB.getInstance();
+        Referee ref = new Referee("naama","naama123", Enums.ActivationStatus.ACTIVE,Enums.UserType.Referee,Enums.RefereeLevel.Primary);
+        instance.delete(ref);
         System.out.println(instance.getAll());
         //instance.save(ref);
-        String[] params = new String[]{"naama","naama!!!",Enums.ActivationStatus.INACTIVE.toString(),Enums.RefereeLevel.Primary.toString()};
+        String[] params = new String[]{"naama","naama!!!",Enums.UserType.Referee.toString(),Enums.ActivationStatus.INACTIVE.toString()};
         instance.update(ref,params);
         System.out.println(instance.getAll());
 
