f8f3797e7a307ee546b13785e3af3e6bca0ee30f
Darabu273 <dAr8194765>
2022-05-11 18:12:06 +0300

    fix testReferee mistakes

--- src/Test/UnitTesting/RefereeTest.java
+++ src/Test/UnitTesting/RefereeTest.java
@@ -17,26 +17,26 @@ public class RefereeTest {
     public void testNewRefereeRegistration(){
         // happy flow
         Enums.ActionStatus status = controller.SignNewReferee("NammaBaruch","naamaBru123@post.ac.il", Enums.RefereeLevel.Secondary);
-        assertEquals("Registration failed", status, Enums.ActionStatus.SUCCESS);
+        assertEquals("Registration failed - should be a successful registration.", status, Enums.ActionStatus.SUCCESS);
 
         // check that we handled the case of null parameters - wrong input
-        status = controller.SignNewReferee("NaamaBaruch","", Enums.RefereeLevel.Primary);
-        assertEquals("Wrong input parameters - email can't be null", status, Enums.ActionStatus.WRONG_PARAMETERS);
+        status = controller.SignNewReferee("NaamaBaruch","", Enums.RefereeLevel.Secondary);
+        assertEquals("Should be wrong in input parameters - email can't be null.", status, Enums.ActionStatus.WRONG_PARAMETERS);
 
-        status = controller.SignNewReferee("","naamaBru123h@post.ac.il", Enums.RefereeLevel.Primary);
-        assertEquals("Wrong input parameters - name can't be null", status, Enums.ActionStatus.WRONG_PARAMETERS);
+        status = controller.SignNewReferee("","naamaBru123h@post.ac.il", Enums.RefereeLevel.Secondary);
+        assertEquals("Should be wrong input parameters - name can't be null", status, Enums.ActionStatus.WRONG_PARAMETERS);
 
         // check that we handled the case of unknown level - wrong input
         status = controller.SignNewReferee("NammaBaruch","naamaBru123@post.ac.il", null);
-        assertEquals("Wrong input parameters - level can't be null", status, Enums.ActionStatus.WRONG_PARAMETERS);
+        assertEquals("Should be wrong input parameters - level can't be null", status, Enums.ActionStatus.WRONG_PARAMETERS);
 
         // check that we handle the case of legal email, example:'@'
         status = controller.SignNewReferee("NammaBaruch","naamaBru123post.ac.il", Enums.RefereeLevel.Secondary);
-        assertEquals("Wrong input parameters - illegal email without '@'", status, Enums.ActionStatus.WRONG_PARAMETERS);
+        assertEquals("Should be exception - illegal email without '@'", status, Enums.ActionStatus.WRONG_PARAMETERS);
 
         // Register referee that existing already in the system -  referee already registered.
         status = controller.SignNewReferee("NammaBaruch","naamaBru123@post.ac.il", Enums.RefereeLevel.Secondary);
-        assertEquals("This user is already registered in", status, Enums.ActionStatus.FAIL);
+        assertEquals("This user should be is already registered in", status, Enums.ActionStatus.FAIL);
 
     }
 
