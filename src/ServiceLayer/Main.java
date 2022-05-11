461eb0e74c28ef900ab0c59b47c9e0dba357c65f
Naama Baruch <t-nbaruch@microsoft.com>
2022-05-11 10:31:23 +0300

    before deletion

--- src/ServiceLayer/Main.java
+++ src/ServiceLayer/Main.java
@@ -1,15 +1,17 @@
 package ServiceLayer;
 
-// todo : ask maxim on the main loop  , and if it's ok to start with some objects in the system, and if we need to implement only the 3 UC or all functions?
-// todo: like - reports, complaints, log, sign in for all users, remove users, see their details?..
-
-// todo: At least one system admin - must be a user (passed the registration process).
+/*
+* //
+* ask maxim on the main loop  , and if it's ok to start with some objects in the system,
+* and if we need to implement only the 3 UC or all functions?
+like - reports, complaints, log, sign in for all users, remove users, see their details?..
+*
+*
+* do we need to handle Sign In UC, or only log in?
+// We didn't handle sign in UC, only log in and sign in for FAN type (guests only)
+* */
 
 // todo - start with some employs, teams (and what needed for the 3 UC) in the system DB - can't sign in from this main
-// todo - write that we didn't handle sign in UC, only log in and sign in for FAN type (guests only)
-
-//todo test all cases! include wrong input parameters
-
 // todo - compare to task 2 - write all differences at the end
 
 import DomainLayer.Enums;
@@ -20,6 +22,7 @@ public class Main {
 
     public static void main(String[] args) {
         //todo : At this point make sure there is at least one signed-in system admin (undergone a registration process).
+        // this admin must be a user  (passed the registration process).
 
         SystemController systemController = SystemController.getInstance();
 
