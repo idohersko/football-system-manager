626ab6e44faf7114892bcfb60ac092654d313f58
idohersko <idohersko@gmail.com>
2022-05-11 19:39:07 +0300

    team user and referee database updated

--- src/DataLayer/DB.java
+++ src/DataLayer/DB.java
@@ -13,4 +13,7 @@ public interface DB <T> {
     void update(T t, String[] params) throws SQLException;
 
     void delete(T t);
+
+
+    //todo - in many function, i have done some system out prints, will it fuck up the tests ? is it right ?
 }
