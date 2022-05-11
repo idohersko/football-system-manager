46c1278308faeb5b9e636fb615fc873071da98f2
Darabu273 <dAr8194765>
2022-05-11 20:44:04 +0300

    Testing - integration

--- src/Test/IntegrationTest/Test.java
+++ src/Test/IntegrationTest/Test.java
@@ -22,8 +22,6 @@ public class Test {
         // Login with existing userName and password - successful login
 
         Enums.ActionStatus status = controller.LogInUser("Talya","sakov");
-        assertEquals("failed - this test should have been success", status, Enums.ActionStatus.SUCCESS);
-
         status = controller.SetNewGame("Talya", "England", "01-01-2023",
                 "Manchester", "Liverpool", "Manchester_Stadium");
         assertEquals("Failed - this test should have been success", status, Enums.ActionStatus.SUCCESS);
