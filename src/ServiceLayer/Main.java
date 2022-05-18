765df3017b70d230491251cc1a5baefb1c97d392
idohersko <idohersko@gmail.com>
2022-05-18 17:09:07 +0300

    fix verify ststem admin test

--- src/ServiceLayer/Main.java
+++ src/ServiceLayer/Main.java
@@ -4,6 +4,7 @@ import DomainLayer.Enums;
 import DomainLayer.Users.AUser;
 import DomainLayer.Users.Referee;
 
+import javax.swing.plaf.synth.SynthDesktopIconUI;
 import java.sql.SQLException;
 import java.util.Scanner;
 
@@ -11,16 +12,18 @@ public class Main {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) throws SQLException {
-        UsersDB instance = UsersDB.getInstance();
-        Referee ref = new Referee("naama","naama123", Enums.ActivationStatus.ACTIVE,Enums.UserType.Referee,Enums.RefereeLevel.Primary);
-        instance.delete(ref);
-        System.out.println(instance.getAll());
-        //instance.save(ref);
-        String[] params = new String[]{"naama","naama!!!",Enums.UserType.Referee.toString(),Enums.ActivationStatus.INACTIVE.toString()};
-        instance.update(ref,params);
-        System.out.println(instance.getAll());
-
-        //instance.save(ref);
+        SystemController controller = SystemController.getInstance();
+        controller.VerifySystemAdmin();
+//        UsersDB instance = UsersDB.getInstance();
+//        //Referee ref = new Referee("naama","naama123", Enums.ActivationStatus.ACTIVE,Enums.UserType.Referee,Enums.RefereeLevel.Primary);
+//        //instance.delete(ref);
+//        System.out.println(instance.getAll());
+//        //instance.save(ref);
+////        String[] params = new String[]{"naama","naama!!!",Enums.UserType.Referee.toString(),Enums.ActivationStatus.INACTIVE.toString()};
+////        instance.update(ref,params);
+////        System.out.println(instance.getAll());
+//
+//        //instance.save(ref);
     }
 
 }
